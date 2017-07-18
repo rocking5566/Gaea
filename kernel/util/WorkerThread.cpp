6b7c8cf81f68eae5219e5d752335f111675cda8a
rocking <rocking_lai@nuuo.com>
2017-07-18 10:38:44 +0800

    [What] Add worker thread pattern

---
+++ kernel/util/WorkerThread.cpp
@@ -0,0 +1,73 @@
+#include "WorkerThread.h"
+
+CWorkerThread::CWorkerThread(QObject* parent)
+    : QThread(parent)
+    , m_bQuit(false)
+{
+    start();
+}
+
+CWorkerThread::~CWorkerThread()
+{
+    m_bQuit = true;
+    m_WorkingCondition.wakeOne();
+
+    while (isRunning())
+    {
+        wait(100);
+    }
+}
+
+void CWorkerThread::run()
+{
+    while (!m_bQuit)
+    {
+        {
+            // Go to sleep if there's nothing to do.
+            QMutexLocker locker(&m_RequestsMutex);
+            while (!m_bQuit && m_Requests.empty())
+            {
+                m_WorkingCondition.wait(&m_RequestsMutex);
+            }
+        }
+
+        // Process pending requests
+        m_RequestsMutex.lock();
+        m_HandlingRequests = m_Requests;
+        m_Requests.clear();
+        m_RequestsMutex.unlock();
+
+        while ((!m_HandlingRequests.empty()) && (!m_bQuit))
+        {
+            SRequest request = m_HandlingRequests.front();
+            m_HandlingRequests.pop_front();
+            HandleRequest(request);
+        }
+    }
+}
+
+void CWorkerThread::HandleRequest(SRequest request)
+{
+    switch (request.m_RequestType)
+    {
+    case RequestType_None:
+        break;
+
+    default:
+        break;
+    }
+}
+
+void CWorkerThread::PushRequestToQueue(RequestType type, QVariant iParam1, void *pvParam2)
+{
+    SRequest request;
+    request.m_RequestType = type;
+    request.m_Param1 = iParam1;
+    request.m_pvParam2 = pvParam2;
+
+    m_RequestsMutex.lock();
+    m_Requests.push_back(request);
+    m_WorkingCondition.wakeOne();
+    m_RequestsMutex.unlock();
+}
+
---
+++ kernel/util/WorkerThread.h
@@ -0,0 +1,49 @@
+#ifndef WorkerThread_h__
+#define WorkerThread_h__
+
+#include <QThread>
+#include <QVariant>
+#include <QMutex>
+#include <QWaitCondition>
+#include <QWaitCondition>
+#include <list>
+
+#define RequestType_None 0
+typedef int RequestType;
+
+struct SRequest
+{
+    int m_RequestType;
+    QVariant m_Param1;
+    void *m_pvParam2;
+
+    SRequest()
+    {
+        m_RequestType = RequestType_None;
+        m_Param1 = 0;
+        m_pvParam2 = NULL;
+    }
+};
+
+class CWorkerThread : public QThread
+{
+    Q_OBJECT
+
+public:
+    CWorkerThread(QObject* parent = NULL);
+    virtual ~CWorkerThread();
+
+protected:
+    void run();
+    void PushRequestToQueue(RequestType type, QVariant iParam1, void *pvParam2);
+
+    virtual void HandleRequest(SRequest request);
+
+private:
+    bool m_bQuit;
+    QMutex m_RequestsMutex;
+    QWaitCondition m_WorkingCondition;
+    std::list<SRequest> m_Requests;
+    std::list<SRequest> m_HandlingRequests;
+};
+#endif // WorkerThread_h__
