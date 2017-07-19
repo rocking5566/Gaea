04383d1fd5edf8ce6b4d46f426ff8ba628a183a2
rocking5566 <aa155495.cs01g@nctu.edu.tw>
2017-07-20 01:02:30 +0800

    [What] Control LibVLC to get RTSP stream

--- kernel/stream/RtspHelper.cpp
+++ kernel/stream/RtspHelper.cpp
@@ -1,17 +1,165 @@
 #include "RtspHelper.h"
+#include "util/Util.h"
+
+using namespace std;
 
 CRtspHelper::CRtspHelper()
+    : m_vlcInstance(NULL)
+    , m_vlcMediaPlayer(NULL)
+    , m_pvlcMedia(NULL)
 {
+    char const* vlc_args[] =
+    {
+        "-I",
+        "dummy",
+        "--ignore-config",
+    };
 
+    m_vlcInstance = libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]), vlc_args);
+    m_vlcMediaPlayer = libvlc_media_player_new(m_vlcInstance);
+    libvlc_video_set_callbacks(m_vlcMediaPlayer, VideoLockCallback, VideoUnlockCallback, VideoDisplayCallback, this);
+    memset (&m_profile, 0, sizeof(SVideoProfile));
 }
 
 CRtspHelper::~CRtspHelper()
 {
-
+    libvlc_media_release(m_pvlcMedia);
+    libvlc_media_player_stop(m_vlcMediaPlayer);
+    libvlc_media_player_release(m_vlcMediaPlayer);
+    libvlc_release(m_vlcInstance);
 }
 
 void CRtspHelper::SetUrl(const char* sUrl)
 {
-    m_sUrl = sUrl;
+    if (m_pvlcMedia != NULL)
+    {
+        libvlc_media_release(m_pvlcMedia);
+    }
+
+    m_pvlcMedia = libvlc_media_new_location(m_vlcInstance, sUrl);
+    libvlc_media_player_set_media(m_vlcMediaPlayer, m_pvlcMedia);
 }
+
+void CRtspHelper::Play()
+{
+    libvlc_state_t state = libvlc_media_player_get_state(m_vlcMediaPlayer);
+
+    if (state == libvlc_Playing)
+    {
+        return;
+    }
+
+    libvlc_media_player_play(m_vlcMediaPlayer);
+
+    // [workaround]: I don't know how to change format (libvlc_video_set_format) after playing...
+    // reconnect the stream
+    if (WaitConnection() && TestAndGetVlcTrack())
+    {
+        libvlc_media_player_stop(m_vlcMediaPlayer);
+        libvlc_video_set_format(m_vlcMediaPlayer, "RV32", m_profile.m_width, m_profile.m_height, m_profile.m_width << 2);
+        libvlc_media_player_play(m_vlcMediaPlayer);
+    }
+}
+
+void CRtspHelper::Pause()
+{
+    libvlc_media_player_pause(m_vlcMediaPlayer);
+}
+
+void CRtspHelper::TearDown()
+{
+    libvlc_media_player_stop(m_vlcMediaPlayer);
+}
+
+void CRtspHelper::RegisterDecodeVideoCallback(DecodeVideoCb videoCb)
+{
+    m_CbDecodeVideo = videoCb;
+}
+
+bool CRtspHelper::TestAndGetVlcTrack()
+{
+    libvlc_media_track_t **tracks = NULL;
+    bool bFindResolution = false;
+    int iTestNum = 0;
+
+    while (!bFindResolution)
+    {
+        if (iTestNum++ > 200)
+        {
+            break;
+        }
+
+        unsigned tracksCount = libvlc_media_tracks_get(m_pvlcMedia, &tracks);
+
+        if (tracks != NULL)
+        {
+            for (unsigned i = 0; i < tracksCount; i++)
+            {
+                if (tracks[i]->i_type == libvlc_track_video
+                    && tracks[i]->video->i_height != 0
+                    && tracks[i]->video->i_width != 0)
+                {
+                    m_profile.m_width = tracks[i]->video->i_width;
+                    m_profile.m_height = tracks[i]->video->i_height;
+                    bFindResolution = true;
+                    break;
+                }
+            }
+        }
+
+        libvlc_media_tracks_release(tracks, tracksCount);
+        MSleep(100);
+    }
+
+    return bFindResolution;
+}
+
+bool CRtspHelper::WaitConnection()
+{
+    libvlc_state_t state = libvlc_media_player_get_state(m_vlcMediaPlayer);
+    int waitCount = 0;
+
+
+    while (state == libvlc_NothingSpecial || state == libvlc_Opening)
+    {
+        if (waitCount++ > 200)
+        {
+            break;
+        }
+
+        state = libvlc_media_player_get_state(m_vlcMediaPlayer);
+        MSleep(100);
+    }
+
+    if (state == libvlc_NothingSpecial || state == libvlc_Opening || state == libvlc_Error)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void* CRtspHelper::VideoLockCallback(void *opaque, void **planes)
+{
+    // TODO - alloc the frame buffer
+    Q_UNUSED(opaque);
+    Q_UNUSED(planes);
+
+    return NULL;
+}
+
+void CRtspHelper::VideoUnlockCallback(void *opaque, void *picture, void *const *planes)
+{
+    // TODO
+    Q_UNUSED(opaque);
+    Q_UNUSED(picture);
+    Q_UNUSED(planes);
+}
+
+void CRtspHelper::VideoDisplayCallback(void *opaque, void *picture)
+{
+    Q_UNUSED(opaque);
+    Q_UNUSED(picture);
+}
+
 
--- kernel/stream/RtspHelper.h
+++ kernel/stream/RtspHelper.h
@@ -2,6 +2,16 @@
 #define RtspHelper_h__
 
 #include <QString>
+#include "vlc/vlc.h"
+
+typedef void *(*DecodeVideoCb)(void *context, void *frame);
+
+struct SVideoProfile
+{
+    unsigned int m_bitrate;
+    unsigned int m_height;
+    unsigned int m_width;
+};
 
 class CRtspHelper
 {
@@ -10,8 +20,27 @@ public:
     ~CRtspHelper();
 
     void SetUrl(const char* sUrl);
+    void Play();
+    void Pause();
+    void TearDown();
+
+    void RegisterDecodeVideoCallback(DecodeVideoCb videoCb);
 
 private:
-    QString m_sUrl;
+    bool TestAndGetVlcTrack();
+    bool WaitConnection();
+
+    //libvlc callback
+    static void* VideoLockCallback(void *opaque, void **planes);
+    static void VideoUnlockCallback(void *opaque, void *picture, void *const *planes);
+    static void VideoDisplayCallback(void *opaque, void *picture);
+
+private:
+    DecodeVideoCb m_CbDecodeVideo;
+
+    libvlc_instance_t* m_vlcInstance;
+    libvlc_media_player_t* m_vlcMediaPlayer;
+    libvlc_media_t* m_pvlcMedia;
+    SVideoProfile m_profile;
 };
 #endif // RtspHelper_h__
