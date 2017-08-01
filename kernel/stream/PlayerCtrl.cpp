25ce43554a321370f9a40840b21b9359f68167db
rocking5566 <aa155495.cs01g@nctu.edu.tw>
2017-08-02 00:38:25 +0800

    [What] Implement CPlayCtrl 1. Connect 2. Disconnect 3. Attach / detach stream callback

--- kernel/stream/PlayerCtrl.cpp
+++ kernel/stream/PlayerCtrl.cpp
@@ -1,13 +1,123 @@
 #include "PlayerCtrl.h"
+#include "StreamCtrl.h"
 
-CPlayCtrl::CPlayCtrl(QObject* parent)
+#define RequestType_Connect 1
+#define RequestType_DisConnect 2
+
+CPlayerCtrl::CPlayerCtrl(QObject* parent)
     : CWorkerThread(parent)
+    , m_iStreamNextID(0)
 {
 
 }
 
-CPlayCtrl::~CPlayCtrl()
+CPlayerCtrl::~CPlayerCtrl()
 {
 
 }
 
+void CPlayerCtrl::AttachStream(int iStreamID, VideoCb videoCb, void* pListener)
+{
+    if (m_mapIdToStreamData.contains(iStreamID))
+    {
+        SStreamCallbackData cbData;
+        cbData.m_cb = videoCb;
+        cbData.m_pListener = pListener;
+
+        m_mapIdToStreamData[iStreamID].m_callbackList << cbData;
+    }
+}
+
+void CPlayerCtrl::DetachStream(int iStreamID, void* pListener)
+{
+    if (m_mapIdToStreamData.contains(iStreamID))
+    {
+        SPlayerStreamData& rStreamData = m_mapIdToStreamData[iStreamID];
+
+        for (auto iter = rStreamData.m_callbackList.begin(); iter != rStreamData.m_callbackList.end(); ++iter)
+        {
+            if (iter->m_pListener == pListener)
+            {
+                rStreamData.m_callbackList.erase(iter);
+                break;
+            }
+        }
+
+    }
+}
+
+void CPlayerCtrl::StreamCallback(void *_this, CStreamCtrl* pSrcStream, CVideoFrame frame)
+{
+    CPlayerCtrl* This = (CPlayerCtrl*)_this;
+    int streamID = pSrcStream->GetStreamID();
+
+    if (This->m_mapIdToStreamData.contains(streamID))
+    {
+        const QList<SStreamCallbackData>& rCallbackList = This->m_mapIdToStreamData[streamID].m_callbackList;
+        foreach (SStreamCallbackData cbData, rCallbackList)
+        {
+            cbData.m_cb(cbData.m_pListener, streamID, frame);
+        }
+    }
+    else
+    {
+        // TODO - error handle
+        // detach stream
+    }
+}
+
+void CPlayerCtrl::HandleRequest(SRequest request)
+{
+    switch (request.m_RequestType)
+    {
+    case RequestType_None:
+        break;
+    case RequestType_Connect:
+        OnConnect(request.m_Param1.toInt());
+        break;
+    case RequestType_DisConnect:
+        DisConnect(request.m_Param1.toInt(), false);
+    default:
+        break;
+    }
+}
+
+int CPlayerCtrl::Connect(const SConnectInfo& rInfo)
+{
+    m_StreamsLock.lockForWrite();
+
+    int iStreamID = m_iStreamNextID++;
+    SPlayerStreamData streamData;
+    streamData.m_StreamInfo = rInfo;
+    streamData.m_Stream = new CStreamCtrl(iStreamID);
+    m_mapIdToStreamData[iStreamID] = streamData;
+    PushRequestToQueue(RequestType_Connect, iStreamID, NULL);
+
+    m_StreamsLock.unlock();
+
+    return iStreamID;
+}
+
+void CPlayerCtrl::OnConnect(int iStreamID)
+{
+    SPlayerStreamData& rStreamData = m_mapIdToStreamData[iStreamID];
+    rStreamData.m_Stream->Connect(rStreamData.m_StreamInfo);
+    rStreamData.m_Stream->Attach(StreamCallback, this);
+}
+
+void CPlayerCtrl::DisConnect(int iStreamID, bool bIsAsync)
+{
+    if (bIsAsync)
+    {
+        PushRequestToQueue(RequestType_DisConnect, iStreamID, NULL);
+    }
+    else
+    {
+        if (m_mapIdToStreamData.contains(iStreamID))
+        {
+            m_mapIdToStreamData[iStreamID].m_Stream->Detach(this);
+            m_mapIdToStreamData[iStreamID].m_Stream->DisConnect();
+            m_mapIdToStreamData.remove(iStreamID);
+        }
+    }
+}
--- kernel/stream/PlayerCtrl.h
+++ kernel/stream/PlayerCtrl.h
@@ -1,17 +1,64 @@
 #ifndef PlayerCtrl_h__
 #define PlayerCtrl_h__
 
+#include <QReadWriteLock>
 #include "util/WorkerThread.h"
+#include "def/KernelTypes.h"
+#include "VideoFrame.h"
+
+class CStreamCtrl;
+typedef void(*VideoCb)(void *pListener, int streamId, CVideoFrame frame);
 
 /*!
     This class provide interfaces to play multiple streams simultaneously.
 */
-class CPlayCtrl : public CWorkerThread
+class CPlayerCtrl : public CWorkerThread
 {
     Q_OBJECT
 
 public:
-    CPlayCtrl(QObject* parent = NULL);
-    ~CPlayCtrl();
+    CPlayerCtrl(QObject* parent = NULL);
+    ~CPlayerCtrl();
+
+    void AttachStream(int iStreamID, VideoCb videoCb, void* pListener);
+    void DetachStream(int iStreamID, void* pListener);
+
+    int Connect(const SConnectInfo& rInfo);     // non-blocking API
+    void DisConnect(int iStreamIndex, bool bIsAsync);
+
+private:
+    static void StreamCallback(void *_this, CStreamCtrl* pSrcStream, CVideoFrame frame);
+
+    // override CWorkerThread
+    virtual void HandleRequest(SRequest request);
+
+    void OnConnect(int iStreamID);
+
+    struct SStreamCallbackData
+    {
+        SStreamCallbackData()
+            : m_cb(NULL)
+            , m_pListener(NULL)
+        {
+        };
+        VideoCb m_cb;
+        void* m_pListener;
+    };
+
+    struct SPlayerStreamData
+    {
+        SPlayerStreamData()
+            : m_Stream(NULL)
+        {
+        };
+        SConnectInfo m_StreamInfo;
+        CStreamCtrl* m_Stream;
+        QList<SStreamCallbackData> m_callbackList;
+    };
+
+private:
+    QReadWriteLock m_StreamsLock;
+    int m_iStreamNextID;
+    QMap<int, SPlayerStreamData> m_mapIdToStreamData;   //ID -> SPlayerStreamData
 };
 #endif // PlayerCtrl_h__
