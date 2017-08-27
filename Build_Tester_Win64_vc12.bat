9c195f90b9253ea17d9f5d3d763897acb5377e05
rocking5566 <aa155495.cs01g@nctu.edu.tw>
2017-08-27 22:11:23 +0800

    [What] Remove redundant script [Why] Build_StreamEngine_Win64_vc12.bat will also build Tester

--- Build_Tester_Win64_vc12.bat
+++
@@ -1,7 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
-IF NOT EXIST build mkdir build
-IF NOT EXIST build\Tester mkdir build\Tester
-
-cd build\Tester
-qmake.exe -spec win32-msvc2013 "CONFIG += x86_64" -r ../../Tester/Tester.pro
-nmake
