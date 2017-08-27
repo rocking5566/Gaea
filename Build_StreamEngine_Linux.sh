40dac38e95724fa7c88b986eb74fa52d2fe4b7ea
rocking5566 <aa155495.cs01g@nctu.edu.tw>
2017-08-27 19:37:41 +0800

    [What] 1. Move Tester to the StreamEngine subfolder 2. Merge build script of StreamEngine and Tester

--- Build_StreamEngine_Linux.sh
+++ Build_StreamEngine_Linux.sh
@@ -1,4 +1,10 @@
 mkdir -p build/StreamEngine
 cd build/StreamEngine
+
+# Stream Engine SDK
 /usr/bin/qmake ../../StreamEngine/StreamEngine.pro -r -spec  linux-g++
+make
+
+# Stream Engine Tester
+/usr/bin/qmake ../../StreamEngine/Tester/Tester.pro -r -spec  linux-g++
 make
--- Build_Tester_Linux.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p build/StreamEngine
-cd build/StreamEngine
-/usr/bin/qmake ../../Tester/Tester.pro -r -spec  linux-g++
-make
