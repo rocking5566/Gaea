494238f052f08f065a0a53978b84eeb103dae288
rocking5566 <aa155495.cs01g@nctu.edu.tw>
2017-09-02 20:05:22 +0800

    [What] 1. Implement basic ui 2. Not interact with model yet 3. not implement stream yet

--- VASystem/src/Device/ConfigDeviceWidget.cpp
+++ VASystem/src/Device/ConfigDeviceWidget.cpp
@@ -2,8 +2,11 @@
 
 CConfigDeviceWidget::CConfigDeviceWidget(QWidget *parent /*= 0*/)
     : QWidget(parent)
+    , m_curStreamId(-1)
 {
     m_ui.setupUi(this);
+    ConnectUISignal();
+    InitTreeWidget();
 }
 
 CConfigDeviceWidget::~CConfigDeviceWidget()
@@ -11,3 +14,95 @@ CConfigDeviceWidget::~CConfigDeviceWidget()
 
 }
 
+void CConfigDeviceWidget::InitTreeWidget()
+{
+    // [TODO] Get data from CDeviceModel
+}
+
+void CConfigDeviceWidget::SetDeviceEntityUIEnable(bool bEnable)
+{
+    m_ui.leName->setEnabled(bEnable);
+    m_ui.leUrl->setEnabled(bEnable);
+    m_ui.lePort->setEnabled(bEnable);
+    m_ui.leUser->setEnabled(bEnable);
+    m_ui.lePassword->setEnabled(bEnable);
+    m_ui.renderWidget->setEnabled(bEnable);
+    m_ui.btnPlay->setEnabled(bEnable);
+}
+
+void CConfigDeviceWidget::ConnectUISignal()
+{
+    connect(m_ui.btnAdd, SIGNAL(clicked()), this, SLOT(OnAddDevice()));
+    connect(m_ui.btnRemove, SIGNAL(clicked()), this, SLOT(OnRemoveDevice()));
+    connect(m_ui.treeWidget, SIGNAL(itemSelectionChanged()), this, SLOT(OnTreeWidgetSelectionChanged()));
+    connect(m_ui.leName, SIGNAL(textChanged(const QString &)), this, SLOT(OnNameChanged(const QString &)));
+
+    connect(m_ui.leName, SIGNAL(editingFinished()), this, SLOT(OnEditingFinished()));
+    connect(m_ui.leUrl, SIGNAL(editingFinished()), this, SLOT(OnEditingFinished()));
+    connect(m_ui.lePort, SIGNAL(editingFinished()), this, SLOT(OnEditingFinished()));
+    connect(m_ui.leUser, SIGNAL(editingFinished()), this, SLOT(OnEditingFinished()));
+    connect(m_ui.lePassword, SIGNAL(editingFinished()), this, SLOT(OnEditingFinished()));
+
+    connect(m_ui.btnPlay, SIGNAL(clicked()), this, SLOT(OnBtnPlayClicked()));
+}
+
+void CConfigDeviceWidget::OnAddDevice()
+{
+    int count = m_ui.treeWidget->topLevelItemCount();
+    QString itemName = "Device_" + QString::number(count);
+    QTreeWidgetItem* pItem = new QTreeWidgetItem(QStringList(itemName));
+
+    // [TODO] update DeviceModel
+    m_ui.treeWidget->addTopLevelItem(pItem);
+}
+
+void CConfigDeviceWidget::OnRemoveDevice()
+{
+    QList<QTreeWidgetItem *> itemList = m_ui.treeWidget->selectedItems();
+
+    if (!itemList.empty())
+    {
+        // [TODO] update DeviceModel
+        SAFE_DELETE(itemList[0]);
+    }
+}
+
+void CConfigDeviceWidget::OnTreeWidgetSelectionChanged()
+{
+    QList<QTreeWidgetItem *> itemList = m_ui.treeWidget->selectedItems();
+
+    if (!itemList.empty())
+    {
+        SetDeviceEntityUIEnable(true);
+        // [TODO] Get data from DeviceModel
+    }
+    else
+    {
+        SetDeviceEntityUIEnable(false);
+    }
+}
+
+void CConfigDeviceWidget::OnNameChanged(const QString& rText)
+{
+    QList<QTreeWidgetItem *> itemList = m_ui.treeWidget->selectedItems();
+
+    if (!itemList.empty())
+    {
+        itemList[0]->setText(0, rText);
+    }
+}
+
+void CConfigDeviceWidget::OnEditingFinished()
+{
+    QList<QTreeWidgetItem *> itemList = m_ui.treeWidget->selectedItems();
+
+    if (!itemList.empty())
+    {
+        // [TODO] update DeviceModel
+    }
+}
+
+void CConfigDeviceWidget::OnBtnPlayClicked()
+{
+    // [TODO] Testing the RTSP 
+}
--- VASystem/src/Device/ConfigDeviceWidget.h
+++ VASystem/src/Device/ConfigDeviceWidget.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include "ui_ConfigDeviceWidget.h"
+#include "PlayerCtrl.h"
 
 class CConfigDeviceWidget : public QWidget
 {
@@ -12,8 +13,22 @@ public:
     explicit CConfigDeviceWidget(QWidget *parent = 0);
     ~CConfigDeviceWidget();
 
+private slots:
+    void OnAddDevice();
+    void OnRemoveDevice();
+    void OnTreeWidgetSelectionChanged();
+    void OnNameChanged(const QString& rText);
+    void OnEditingFinished();
+    void OnBtnPlayClicked();
+
+private:
+    void ConnectUISignal();
+    void InitTreeWidget();
+    void SetDeviceEntityUIEnable(bool bEnable);
+
 private:
     Ui::ConfigDeviceWidget m_ui;
-
+    CPlayerCtrl m_pPlayerCtrl;
+    int m_curStreamId;
 };
 #endif // ConfigDeviceWidget_h__
